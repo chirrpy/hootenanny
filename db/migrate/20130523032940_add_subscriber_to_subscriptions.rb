0da0e9d0532171977901799c0de3b18e74312c29
Jeff Felchner <accounts+git@thekompanee.com>
2013-05-23 00:38:31 -0500

    Fix subscriber column on the subscriptions table

--- db/migrate/20130523032940_add_subscriber_to_subscriptions.rb
+++ db/migrate/20130523032940_add_subscriber_to_subscriptions.rb
@@ -1,7 +1,7 @@
 class AddSubscriberToSubscriptions < ActiveRecord::Migration
   def change
-    add_column    :hootenanny_subscriptions, :subscriber, :string, :liimit => 250
-    change_column :hootenanny_subscriptions, :subscriber, :string, :liimit => 250, :null => false
+    add_column    :hootenanny_subscriptions, :subscriber, :string, :limit => 250
+    change_column :hootenanny_subscriptions, :subscriber, :string, :limit => 250, :null => false
 
     add_index :hootenanny_subscriptions, :subscriber,            :unique => false
     add_index :hootenanny_subscriptions, [:subscriber, :topic],  :unique => true
--- spec/dummy/db/migrate/20130523053741_add_subscriber_to_subscriptions.hootenanny.rb
+++ spec/dummy/db/migrate/20130523053741_add_subscriber_to_subscriptions.hootenanny.rb
@@ -1,8 +1,8 @@
 # This migration comes from hootenanny (originally 20130523032940)
 class AddSubscriberToSubscriptions < ActiveRecord::Migration
   def change
-    add_column    :hootenanny_subscriptions, :subscriber, :string, :liimit => 250
-    change_column :hootenanny_subscriptions, :subscriber, :string, :liimit => 250, :null => false
+    add_column    :hootenanny_subscriptions, :subscriber, :string, :limit => 250
+    change_column :hootenanny_subscriptions, :subscriber, :string, :limit => 250, :null => false
 
     add_index :hootenanny_subscriptions, :subscriber,            :unique => false
     add_index :hootenanny_subscriptions, [:subscriber, :topic],  :unique => true
