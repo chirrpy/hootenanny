e737efcdf8b198233b6bfe036375a2ea870c6f9c
Jeff Felchner <accounts+git@thekompanee.com>
2013-05-22 22:48:15 -0500

    Update constant access so we can reference the class under test less verbosely

--- spec/models/hootenanny/subscription_spec.rb
+++ spec/models/hootenanny/subscription_spec.rb
@@ -1,16 +1,18 @@
 require 'rspectacular/spec_helpers/active_record_basic'
 require 'hootenanny/subscription'
 
+Subscription ||= Hootenanny::Subscription
+
 describe Hootenanny::Subscription do
   it 'can find itself based on its topic' do
     existing_subscription = create(:subscription, :topic => 'my_topic')
 
-    found_subscriptions = Hootenanny::Subscription.to('my_topic')
+    found_subscriptions = Subscription.to('my_topic')
     found_subscription  = found_subscriptions.first
 
     expect(found_subscriptions).to have(1).item
 
-    expect(found_subscription).to be_a Hootenanny::Subscription
+    expect(found_subscription).to be_a Subscription
     expect(found_subscription.topic).to eql 'my_topic'
   end
 end
